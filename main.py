34d5a0bc3bc6f20aefabceb04e9758940459c9b6
Cristian <popovici.cristi.pc@gmail.com>
2022-09-05 16:39:46 +0300

    Change to local usage

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
